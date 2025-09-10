df97929fb318d06961cdff214278fb1514a20471
SeungjaeOh <ohsj3781@gmail.com>
2025-09-10 14:18:06 +0900

    Refactor problem 2162

--- 2162/2162.cpp
+++ 2162/2162.cpp
@@ -29,7 +29,7 @@ void MergeSet(int lset, int rset) {
   sets_size[lset] += sets_size[rset];
 
   --cnt_sets;
-  max_set_size = std::max(max_set_size, sets_size[lset]);
+
   return;
 }
 
@@ -37,14 +37,14 @@ std::istream& operator>>(std::istream& is, std::pair<int, int>& p) {
   is >> p.first >> p.second;
   return is;
 }
-
+/*CCW only return 1, 0, -1*/
 const int CCW(const std::pair<int, int>& p0, const std::pair<int, int>& p1,
               const std::pair<int, int>& p2) {
   const int calc_0 =
       p0.first * p1.second + p1.first * p2.second + p2.first * p0.second;
   const int calc_1 =
       p1.first * p0.second + p2.first * p1.second + p0.first * p2.second;
-  return calc_0 - calc_1 == 0 ? 0 : calc_0 - calc_1 > 0 ? 1 : -1;
+  return calc_0 == calc_1 ? 0 : calc_0 > calc_1 ? 1 : -1;
 }
 
 struct Line {
@@ -54,12 +54,19 @@ struct Line {
   const bool Intersect(const Line& l) const {
     const int this_line =
         CCW(this->p0, this->p1, l.p0) * CCW(this->p0, this->p1, l.p1);
+    if (this_line > 0) {
+      return false;
+    }
+
     const int other_line =
         CCW(l.p0, l.p1, this->p0) * CCW(l.p0, l.p1, this->p1);
-    if (this_line == 0 && other_line == 0) {
-      return l.p0 <= this->p1 && this->p0 <= l.p1;
+    if (other_line > 0) {
+      return false;
     }
-    return this_line <= 0 && other_line <= 0;
+
+    return this_line == 0 && other_line == 0
+               ? l.p0 <= this->p1 && this->p0 <= l.p1
+               : true;
   }
 };
 
@@ -90,11 +97,16 @@ int main() {
     if (lines[i].p0 > lines[i].p1) {
       std::swap(lines[i].p0, lines[i].p1);
     }
+
     for (int j = 0; j < i; ++j) {
       if (lines[i].Intersect(lines[j])) {
         MergeSet(i, j);
       }
     }
+  }
+
+  for (int i = 0; i < sets_size.size(); ++i) {
+    max_set_size = std::max(max_set_size, sets_size[i]);
   }
 
   std::cout << cnt_sets << "\n" << max_set_size;
