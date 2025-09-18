94a6838ceafa1432cf169e868e0ff49c99f1550b
SeungJaeOh <ohsj3781@gmail.com>
2025-09-19 06:04:29 +0900

    Add solution for problem 1014

---
+++ BOJ/1014/1014.cpp
@@ -0,0 +1,107 @@
+#include <climits>
+#include <iostream>
+
+int N, M;
+char matrix[10][10];
+int dp[10][1024];
+
+const bool ValidArrange(const int num, const int first) {
+  for (int i = 0; i < M; ++i) {
+    if (!(num & (1 << i))) {
+      continue;
+    }
+
+    if (matrix[first][M - 1 - i] == 'x') {
+      return false;
+    }
+
+    for (int j = std::max(0, i - 1); j <= std::min(M - 1, i + 1); ++j) {
+      if (i == j) {
+        continue;
+      }
+      if ((num >> j) & 1) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const bool ValidLastArrange(const int now, const int before) {
+  for (int i = 0; i < M; ++i) {
+    if ((now >> i) & 1) {
+      for (int j = std::max(0, i - 1); j <= std::min(M - 1, i + 1); ++j) {
+        if (i == j) {
+          continue;
+        }
+        if ((before >> j) & 1) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+const int CountBits(int num) {
+  int cnt = 0;
+  while (num) {
+    cnt += num & 1 ? 1 : 0;
+    num >>= 1;
+  }
+  return cnt;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+  int C;
+  std::cin >> C;
+
+  for (int c = 0; c < C; ++c) {
+    std::cin >> N >> M;
+
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < M; ++j) {
+        std::cin >> matrix[i][j];
+      }
+    }
+
+    const int dp_max = 1 << M;
+    for (int j = 0; j < dp_max; ++j) {
+      if (ValidArrange(j, 0)) {
+        dp[0][j] = CountBits(j);
+      } else {
+        dp[0][j] = INT_MIN;
+      }
+    }
+
+    for (int i = 1; i < N; ++i) {
+      for (int j = 0; j < dp_max; ++j) {
+        dp[i][j] = INT_MIN;
+
+        if (!ValidArrange(j, i)) {
+          continue;
+        }
+        for (int k = 0; k < dp_max; ++k) {
+          if (dp[i - 1][k] == INT_MIN || !ValidLastArrange(j, k)) {
+            continue;
+          }
+
+          dp[i][j] = std::max(dp[i][j], dp[i - 1][k] + CountBits(j));
+        }
+      }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < dp_max; ++i) {
+      ans = std::max(ans, dp[N - 1][i]);
+    }
+
+    std::cout << ans << "\n";
+  }
+
+  return 0;
+}
