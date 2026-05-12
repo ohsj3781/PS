34fd60e96869e9d2b344abc372b45cbdd8d68186
ohsj3781 <ohsj3781@gmail.com>
2026-05-12 16:44:59 +0900

    Add solutio SWEA problem 1263 BitSet BFS

--- 02_SWEA/01_1000-1999/1263/1263.cpp
+++ 02_SWEA/01_1000-1999/1263/1263.cpp
@@ -1,64 +1,82 @@
-#include <algorithm>
+#include <bitset>
 #include <climits>
 #include <iostream>
+#include <sstream>
 
-static constexpr int INF = INT_MAX >> 1;
+static constexpr int INF = INT_MAX;
 static constexpr int MAX_N = 1000;
-int matrix[MAX_N][MAX_N];
-int cost_sum[MAX_N];
+
+int N;
+std::bitset<MAX_N> edges[MAX_N];
+
+int calc_cc(int src) {
+  static std::bitset<MAX_N> visited, cur, next;
+  visited.reset();
+  cur.reset();
+  next.reset();
+
+  visited.set(src); // src는 거리 0, 시작부터 방문 처리
+  cur.set(src);     // 현재 탐색할 노드 집합 (현재 depth의 레이어)
+
+  int cc_sum = 0, depth = 0;
+  while (cur.any()) {
+    ++depth;
+    next.reset();
+
+    for (int i = 0; i < N; ++i) {
+      if (cur[i]) {
+        next |= edges[i];
+      }
+    }
+
+    next &= ~visited;
+    cc_sum += next.count() * depth;
+    visited |= next;
+    cur = next;
+  }
+
+  return cc_sum;
+}
 
 int main() {
   std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr), std::cout.tie(nullptr);
+  std::cin.tie(nullptr);
 
-  //   freopen("input.txt", "r", stdin);
+  // freopen("input.txt", "r", stdin);
 
   int T;
   std::cin >> T;
+  std::cin.ignore();
+
+  std::string str;
+  std::stringstream buffer;
 
   for (int tc = 1; tc <= T; ++tc) {
-    int N;
-    std::cin >> N;
+    std::getline(std::cin, str);
+    buffer.clear();
+    buffer << str;
+
+    buffer >> N;
 
     // init
-    std::fill_n(cost_sum, N, 0);
+    for (int i = 0; i < N; ++i) {
+      edges[i].reset();
+    }
 
-    // get input
+    int input;
     for (int i = 0; i < N; ++i) {
       for (int j = 0; j < N; ++j) {
-        std::cin >> matrix[i][j];
+        buffer >> input;
+        if (input)
+          edges[i].set(j);
       }
     }
 
-    // calc, Floyd-Warshall
-    for (int k = 0; k < N; ++k) {
-      for (int i = 0; i < N; ++i) {
-        if (k == i || matrix[i][k] == 0) {
-          continue;
-        }
-        for (int j = 0; j < N; ++j) {
-          if (k == j || i == j || matrix[k][j] == 0) {
-            continue;
-          }
+    int ans = INF;
+    for (int i = 0; i < N; ++i)
+      ans = std::min(ans, calc_cc(i));
 
-          matrix[i][j] =
-              matrix[i][j] == 0
-                  ? matrix[i][k] + matrix[k][j]
-                  : std::min(matrix[i][j], matrix[i][k] + matrix[k][j]);
-        }
-      }
-    }
-
-    // calc sum
-    for (int i = 0; i < N; ++i) {
-      for (int j = i + 1; j < N; ++j) {
-        cost_sum[i] += matrix[i][j];
-        cost_sum[j] += matrix[i][j];
-      }
-    }
-
-    std::cout << "#" << tc << " " << *std::min_element(cost_sum, cost_sum + N)
-              << "\n";
+    std::cout << "#" << tc << " " << ans << "\n";
   }
 
   return 0;
